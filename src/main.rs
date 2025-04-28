d97c324b22dea86b0899c7f5de125b3d2d5589a0
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-04-27 23:25:37 -0700

    feat: moved discord both stuff to own file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
+mod discord;
+mod minecraft;
+
 extern crate dotenv;
 
 use dotenv::dotenv;
-use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
-
-struct Data {} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-/// Responds with "world!"
-#[poise::command(slash_command)]
-async fn hello(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("world!").await?;
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() {
@@ -20,22 +11,5 @@ async fn main() {
 
     let discord_token = std::env::var("DISCORD_TOKEN").expect("'DISCORD_TOKEN' was not found");
 
-    let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            commands: vec![hello()],
-            ..Default::default()
-        })
-        .setup(|ctx, _ready, framework| {
-            Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
-            })
-        })
-        .build();
-
-    let client = ClientBuilder::new(discord_token, GatewayIntents::non_privileged())
-        .framework(framework)
-        .await;
-    // Ok(client.into())
-    client.unwrap().start().await.unwrap();
+    discord::run_bot(discord_token).await;
 }
