1ed98cfcde53931c77e5f6ad4f8bb755a61f2e24
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-04-27 21:24:27 -0700

    init commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
+
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Responds with "world!"
+#[poise::command(slash_command)]
+async fn hello(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("world!").await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let discord_token = std::env::var("DISCORD_TOKEN").expect("'DISCORD_TOKEN' was not found");
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![hello()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = ClientBuilder::new(discord_token, GatewayIntents::non_privileged())
+        .framework(framework)
+        .await;
+    // Ok(client.into())
+    client.unwrap().start().await.unwrap();
+}
